8b652ff0de4e47bfce5e20e520e08c0fd1f9b931
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2013-11-25 14:07:35 +0100

    Fixed integrationtest

--- net.powermatcher.fpai.integration.test/src/net/powermatcher/fpai/controller/test/PMControllerTest.java
+++ net.powermatcher.fpai.integration.test/src/net/powermatcher/fpai/controller/test/PMControllerTest.java
@@ -56,7 +56,7 @@ public class PMControllerTest extends TestCase {
 
     public PMControllerTest() {
         configuration = new HashMap<String, Object>();
-        configuration.put("applianceIds", "test");
+        configuration.put("resourceIds", "test");
         configuration.put("cluster.id", "ExampleCluster");
         configuration.put("location.id", "ExampleLocation");
         configuration.put("messaging.protocol", "INTERNAL_v1");
@@ -70,6 +70,7 @@ public class PMControllerTest extends TestCase {
         configuration.put("market.maximum.price", "0.99");
         configuration.put("market.price.steps", "100");
         configuration.put("market.significance", "2");
+        configuration.put("small.widget", "true");
     }
 
     private String concentratorId;
