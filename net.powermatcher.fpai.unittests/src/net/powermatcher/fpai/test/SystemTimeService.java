9cec04f49a697f51272ffc9dcafa16edb32e769c
Wilco Wijbrandi <wilco.wijbrandi@tno.nl>
2013-12-02 16:31:39 +0100

    Test

--- net.powermatcher.fpai.unittests/src/net/powermatcher/fpai/test/SystemTimeService.java
+++ net.powermatcher.fpai.unittests/src/net/powermatcher/fpai/test/SystemTimeService.java
@@ -4,15 +4,15 @@ import net.powermatcher.core.scheduler.service.TimeService;
 
 /** Implementation of the TimeService which uses the system clock */
 public class SystemTimeService implements TimeService {
-	@Override
-	public long currentTimeMillis() {
-		// use the system clock
-		return System.currentTimeMillis();
-	}
+    @Override
+    public long currentTimeMillis() {
+        // Use the system clock
+        return System.currentTimeMillis();
+    }
 
-	@Override
-	public int getRate() {
-		// rate is 1:1 to the progress of the system clock
-		return 1;
-	}
+    @Override
+    public int getRate() {
+        // Rate is 1:1 to the progress of the system clock
+        return 1;
+    }
 }
